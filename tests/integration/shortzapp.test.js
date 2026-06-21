5d27582a013787d76f2ae9f5de3e42291be09830
gfloriano11 <gflorianodev@gmail.com>
2026-06-20 21:29:25 -0300

    feat: implement new tests validation and config

--- tests/integration/shortzapp.test.js
+++ tests/integration/shortzapp.test.js
@@ -1,18 +1,33 @@
 import request from 'supertest';
-import { describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import app from '../../app';
+const sequelize = require('../../configuration/database');
+const Video = require('../../modules/video/videoModel');
+
+beforeAll(async () => {
+  await sequelize.sync({ force: true });
+});
+
+afterAll(async () => {
+  await sequelize.close();
+});
+
 
 describe("Testes de integração do ShortzApp", () => {
+
+  const USER = {
+    username: "vitor",
+    fullName: "Vitor Silva",
+    email: "vitor@email.com",
+    password: "123456"
+  };
 
   it("deve cadastrar um usuário através da rota POST /register", async () => {
     const response = await request(app)
       .post('/register')
       .send({
-        username: "vitor",
-        fullName: "Vitor Silva",
-        email: "vitor@email.com",
-        password: "123456",
-        confirmPassword: "123456"
+        ...USER,
+        confirmPassword: USER.password
       });
 
     expect(response.status).toBe(302);
@@ -29,15 +44,33 @@ describe("Testes de integração do ShortzApp", () => {
     expect(response.status).toBe(302);
   });
 
+  it("deve criar um vídeo através da rota POST /create", async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({
+        login: 'vitor@email.com',
+        password: '123456'
+      });
+
+    expect(response.status).toBe(302);
+  });
+
   it("deve permitir comentar um vídeo para um usuário autenticado", async () => {
     const agent = request.agent(app);
 
     await agent
       .post("/login")
       .send({
-        login: "gus",
-        password: "123456"
+        login: USER.email,
+        password: USER.password
       });
+
+    await Video.create({
+      title: 'Vídeo teste',
+      videoPath: 'teste.mp4',
+      thumbnailPath: 'teste.png',
+      userId: 1
+    });
 
     const response = await agent
       .post("/video/1/comment")
@@ -54,12 +87,12 @@ describe("Testes de integração do ShortzApp", () => {
     await agent
       .post("/login")
       .send({
-        login: "gus",
-        password: "123456"
+        login: USER.email,
+        password: USER.password
       });
 
     const response = await agent.post('/video/1/toggle-like')
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(201);
   });
 });
--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    reporters: "verbose"
+    reporters: "verbose",
+    include: [
+      'tests/integration/**/*.test.js'
+    ],
+    exclude: [
+      'tests/e2e/login.test.js',
+      'tests/e2e/register.test.js'
+    ]
   }
 });
